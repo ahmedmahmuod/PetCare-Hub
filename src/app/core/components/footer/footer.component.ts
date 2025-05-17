1085784f8e86e0e948748331186dc9a3dee62e9c
ahmed mahmoud <ahmedmahmuuud@gmail.com>
2025-05-17 15:27:56 +0300

    Fix: header link and footer lonk

--- src/app/core/components/footer/footer.component.ts
+++ src/app/core/components/footer/footer.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import { TranslateModule } from '@ngx-translate/core';
 import { ButtonModule } from 'primeng/button';
 
 @Component({
   selector: 'app-footer',
   standalone: true,
-  imports: [TranslateModule, ButtonModule, TranslateModule],
+  imports: [TranslateModule, ButtonModule, RouterLink],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.css',
 })
